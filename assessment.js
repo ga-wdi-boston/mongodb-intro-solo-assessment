440b15fa2b2861a14ad60333a5de4e206df95746
EShimel <edieshimel@gmail.com>
2015-11-19 09:31:49 -0500

    Mostly notes

--- assessment.js
+++ assessment.js
@@ -15,6 +15,26 @@ db.dropDatabase();
 // House Targaryen, motto 'Fire and Blood'
 
 // your code begins here
+//here's the bulk load
+mongoimport --db=mongo-westeros --collection=houses --type=csv --headerline --file=westeros-data/csv/houses.csv;
+
+//here's the insert
+db.houses.insert([
+  {
+  name: "Arryn",
+  motto: "As High as Honor"
+
+},
+{
+  name: "Targaryen",
+  motto: "Fire and blood"
+
+},
+{
+   name: "Stark",
+  motto: "Winter is Coming"
+]);
+
 
 // your code ends here
 
@@ -32,14 +52,21 @@ db.dropDatabase();
 // Jon Arryn
 
 // your code begins here
+db.houses.update({name : "Stark")},
+{ $push : { people: { first_name : "Ned", surname: "Stark" } , { first_name :  "Arya", surname : "Stark"} { first_name: "Sansa', surname: 'Stark"}}} );
 
+db.houses.update({name : "Targaryen")},
+{ $push : { people: { first_name : "Viserys", surname: "Targaryen" } , { first_name: "Daenerys", surname : "Targaryen"}}} );
+
+db.houses.update({"name" : "Arryn"});
+{ $push : { people: { first_name : "Jon", surname: "Arryn" }}} );
 // your code ends here
 
 
 // Question 3
 //
 // House Arryn is not honorable!  Delete their motto.
-
+db.house.delete({ motto: "As High as Honor"});
 // your code begins here
 
 // your code ends here
@@ -51,7 +78,7 @@ db.dropDatabase();
 // House Stark.
 
 // your code begins here
-
+db.house.delete({Stark});
 // your code ends here
 
 
