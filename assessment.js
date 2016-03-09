f0d0bf0935d770fb618761542e68a4526e3292a8
J-Weeks <weeksjasons@gmail.com>
2016-03-09 08:55:48 -0500

    clarifies readne

--- assessment.js
+++ assessment.js
@@ -21,8 +21,8 @@ db.dropDatabase();
 
 // Question 2
 //
-// Using update and $push, add the following people to the
-// respective houses
+// Using update at least once and $push at least once, add the following people
+// to the respective houses in a "members" array
 //
 // Ned Stark
 // Arya Stark
