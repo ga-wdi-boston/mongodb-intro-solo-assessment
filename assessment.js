88dce1357dfba61609403fda06ec688951356d85
msyao-github <msyaois@gmail.com>
2016-03-09 09:20:43 -0500

    added semicolons

--- assessment.js
+++ assessment.js
@@ -18,15 +18,15 @@ db.dropDatabase();
 db.houses.insert({
   House : "Arryn",
   Motto : "As High as Honor",
-})
+});
 db.houses.insert({
   House : "Stark",
   Motto : "Winter is Coming",
-})
+});
 db.houses.insert({
   House : "Targaryen",
   Motto : "Fire and Blood",
-})
+});
 // your code ends here
 
 
@@ -46,15 +46,15 @@ db.houses.insert({
 db.houses.update(
    { House : "Arryn", },
    { $push: { members: "Jon Arryn" } }
-)
+);
 db.houses.update(
    { House : "Stark", },
    { $push: { members: { $each: [ "Ned Stark", "Arya Stark", "Sansa Stark" ] } } }
-)
+);
 db.houses.update(
    { House : "Targaryen", },
    { $push: { members: { $each: ["Viserys Targaryen", "Daenerys Targaryen" ] } } }
-)
+);
 
 // your code ends here
 
@@ -64,7 +64,7 @@ db.houses.update(
 // House Arryn is not honorable!  Delete their motto.
 
 // your code begins here
-db.houses.remove( {'this.Motto == "As High as Honor"'} )
+db.houses.remove( {'this.Motto == "As High as Honor"'} );
 // your code ends here
 
 
@@ -74,8 +74,8 @@ db.houses.remove( {'this.Motto == "As High as Honor"'} )
 // House Stark.
 
 // your code begins here
-> use houses
-> db.dropDatabase()
+use houses;
+db.dropDatabase();
 // your code ends here
 
 
