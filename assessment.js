d49f75ac35bb7e017852cf9afb90c975f761743c
JenWeber <weberj10@gmail.com>
2016-03-09 09:28:17 -0500

    initial commit of diagnostic

--- assessment.js
+++ assessment.js
@@ -15,7 +15,22 @@ db.dropDatabase();
 // House Targaryen, motto 'Fire and Blood'
 
 // your code begins here
+db.CreateCollection('houses');
 
+db.houses.insert({
+  house: "Arryn",
+  motto: "As High as Honor",
+});
+
+db.houses.insert({
+  house: "Stark",
+  motto: "Winter is Coming",
+});
+
+db.houses.insert({
+  house: "Targaryen",
+  motto: "Fire and Blood",
+});
 // your code ends here
 
 
@@ -32,6 +47,34 @@ db.dropDatabase();
 // Jon Arryn
 
 // your code begins here
+db.houses.update(
+  {house: "Stark"},
+  {
+    $set:{ members: ["Ned Stark", "Arya Stark", "Sansa Stark"]}
+  },
+  {multi: false}
+);
+
+db.houses.update(
+  {house: "Targaryen"},
+  {
+    $set:{ members: ["Viserys Targaryen"]}
+  },
+  {multi: false}
+);
+
+db.houses.update(
+   { house: "Targaryen" },
+   { $push: { members: "Daenerys Targaryen" } }
+)
+
+db.houses.update(
+  {house: "Arryn"},
+  {
+    $set:{ members: ["Jon Arryn"]}
+  },
+  {multi: false}
+);
 
 // your code ends here
 
@@ -41,7 +84,13 @@ db.dropDatabase();
 // House Arryn is not honorable!  Delete their motto.
 
 // your code begins here
-
+db.houses.update(
+  {house: "Arryn"},
+  {
+    $set:{ motto: ""}
+  },
+  {multi: false}
+);
 // your code ends here
 
 
@@ -51,7 +100,9 @@ db.dropDatabase();
 // House Stark.
 
 // your code begins here
-
+db.houses.remove(
+  {house: "Stark"}
+);
 // your code ends here
 
 
