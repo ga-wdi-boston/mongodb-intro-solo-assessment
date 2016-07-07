7d88c298b0f4ecf96fc0b6bcee76ff2de06a0592
SarahHartwick <sarah@surfsetfitness.com>
2016-07-07 09:17:09 -0400

    MongoDB CRUD Diagnostic
    Complete all Questions

--- assessment.js
+++ assessment.js
@@ -15,7 +15,18 @@ db.dropDatabase();
 // House Targaryen, motto 'Fire and Blood'
 
 // your code begins here
-
+db.houses.insert({
+  name: "House Arryn",
+  motto: "As High as Honor"
+});
+db.houses.insert({
+  name: "House Stark",
+  motto: "Winter is Coming"
+});
+db.houses.insert({
+  name: "House Targaryen",
+  motto: "Fire and Blood"
+});
 // your code ends here
 
 
@@ -32,7 +43,22 @@ db.dropDatabase();
 // Jon Arryn
 
 // your code begins here
-
+db.houses.updateMany(
+  {},
+  {$set: {members: []}}
+);
+db.houses.update(
+   { name: "House Stark" },
+   { $push: { members: { $each: ["Ned Stark", "Arya Stark", "Sansa Stark"] }}}
+);
+db.houses.update(
+   { name: "House Arryn" },
+   { $push: { members: "Jon Arryn"} }
+);
+db.houses.update(
+   { name: "House Targaryen" },
+   { $push: { members: { $each: ["Viserys Targaryen", "Daenerys Targaryen"] }}}
+);
 // your code ends here
 
 
@@ -41,7 +67,10 @@ db.dropDatabase();
 // House Arryn is not honorable!  Delete their motto.
 
 // your code begins here
-
+db.houses.update(
+   { name: "House Arryn" },
+   { $unset: { motto: ""} }
+);
 // your code ends here
 
 
@@ -51,7 +80,9 @@ db.dropDatabase();
 // House Stark.
 
 // your code begins here
-
+db.houses.remove(
+  { name: "House Stark" }
+);
 // your code ends here
 
 
