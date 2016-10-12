a8fb7ad12fc967c6c5f0977b9f460148535f9f5d
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-12 09:08:22 -0400

    Add CRUD operations to diagnostic.js
    Add, update, and remove houses

--- diagnostic.js
+++ diagnostic.js
@@ -16,6 +16,21 @@ db.dropDatabase();
 
 // your code begins here
 
+db.houses.insert([
+  {
+    name: "House Arryn",
+    motto: "As High As Honor"
+  },
+  {
+    name: "House Stark",
+    motto: "Winter is Coming"
+  },
+  {
+    name: "House Targaryen",
+    motto: "Fire and Blood"
+  },
+]);
+
 // your code ends here
 
 
@@ -33,6 +48,21 @@ db.dropDatabase();
 
 // your code begins here
 
+db.houses.update(
+  { name: "House Stark" },
+  { $push: { members: ["Ned Stark", "Arya Stark", "Sansa Stark"] } }
+);
+
+db.houses.update(
+  { name: "House Targaryen" },
+  { $push: { members: ["Viserys Targaryen", "Daenerys Targaryen"] } }
+);
+
+db.houses.update(
+  { name: "House Arryn" },
+  { $push: { members: ["Jon Arryn"] } }
+);
+
 // your code ends here
 
 
@@ -41,6 +71,11 @@ db.dropDatabase();
 // House Arryn is not honorable!  Delete their motto.
 
 // your code begins here
+
+db.houses.update(
+  { name: "House Arryn" },
+  { $set: { motto: '' } }
+);
 
 // your code ends here
 
@@ -51,6 +86,10 @@ db.dropDatabase();
 // House Stark.
 
 // your code begins here
+
+db.houses.remove(
+  { name: "House Stark" }
+);
 
 // your code ends here
 
