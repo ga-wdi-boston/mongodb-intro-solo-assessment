e25f2ec5fdb376cb17a10269484660723bcf288e
BMorse09 <benmorse09@gmail.com>
2016-10-12 09:23:45 -0400

    Answers for all questions

--- diagnostic.js
+++ diagnostic.js
@@ -16,6 +16,18 @@ db.dropDatabase();
 
 // your code begins here
 
+db.createCollection('houses');
+db.houses.insert({
+  name: 'House Arryn',
+  motto: 'As High as Honor'
+}, {
+  name: 'House Stark',
+  motto: 'Winter is Coming'
+}, {
+  name: 'House Targaryen',
+  motto: 'Fire and Blood'
+});
+
 // your code ends here
 
 
@@ -32,6 +44,24 @@ db.dropDatabase();
 // Jon Arryn
 
 // your code begins here
+db.houses.update(
+    {name: 'House Arryn'},
+  {
+    $push: {members: 'Jon Arryn'}
+  },
+  {
+    {name: 'House Stark'},
+  }
+  {
+    $push: {members: 'Ned Stark, Arya Stark, Sansa Stark'}
+  },
+  {
+    {name: 'House Targaryen'},
+  }
+  {
+    $push: {members: 'Viserys Targaryen, Daenerys Targaryen'}
+  }
+);
 
 // your code ends here
 
@@ -41,7 +71,7 @@ db.dropDatabase();
 // House Arryn is not honorable!  Delete their motto.
 
 // your code begins here
-
+db.houses.remove({motto: 'As High as Honor'})
 // your code ends here
 
 
@@ -51,7 +81,7 @@ db.dropDatabase();
 // House Stark.
 
 // your code begins here
-
+db.houses.remove.({name: 'House Stark'})
 // your code ends here
 
 
