a9980e14ab696970bead024566b050c7c9fc16fb
viohlet <violetavenegas@hotmail.com>
2016-10-12 09:27:37 -0400

    mongodb-crud-diagnostic VV

--- diagnostic.js
+++ diagnostic.js
@@ -15,9 +15,31 @@ db.dropDatabase();
 // House Targaryen, motto 'Fire and Blood'
 
 // your code begins here
-
+const create = function(name, motto) {
+  House.create({
+      name: name,
+      motto: motto
+  })
+  .then(console.log)
+  .catch(console.error)
+  .then(done);
+};
 // your code ends here
 
+db.house.insert({
+  name: "House Arryn",
+  motto: "As High as Honor"
+});
+
+db.house.insert({
+  name: "House Stark",
+  motto: "Winter is coming"
+});
+
+db.house.insert({
+  name: "House Targaryen",
+  motto: "Fire and Blood"
+});
 
 // Question 2
 //
@@ -41,6 +63,7 @@ db.dropDatabase();
 // House Arryn is not honorable!  Delete their motto.
 
 // your code begins here
+db.houses.remove({ 'House Arryn': 'motto'})
 
 // your code ends here
 
@@ -51,6 +74,7 @@ db.dropDatabase();
 // House Stark.
 
 // your code begins here
+db.houses.remove( 'House Stark' )
 
 // your code ends here
 
