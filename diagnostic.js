6d075003ecfbf62e562df392ddca55f332f550fa
akradin <alex.kradin@gmail.com>
2016-12-12 09:29:22 -0500

    Complete diagnostic
    Comfort: 2
    Clarity: 3

--- diagnostic.js
+++ diagnostic.js
@@ -15,7 +15,9 @@ db.dropDatabase();
 // House Targaryen, motto 'Fire and Blood'
 
 // your code begins here
-
+db.houses.insert({"name":"House Arryn", "motto":"As High as Honor"})
+db.houses.insert({"name":"House Stark", "motto":"Winter is Coming" })
+db.houses.insert({"name":"House Targaryen", "motto":"Fire and Blood"})
 // your code ends here
 
 
@@ -32,7 +34,9 @@ db.dropDatabase();
 // Jon Arryn
 
 // your code begins here
-
+db.houses.update({name:"House Stark"}, {$push: {members:  {$each:["Ned Stark", "Arya Stark", "Sansa Stark"]}}})
+db.houses.update({name:"House Targaryen"}, {$push: {members:  {$each:["Viserys Targaryen", "Daenerys Targaryen"]}}})
+db.houses.update({name:"House Arryn"}, {$set: {members:"Jon Arryn"}})
 // your code ends here
 
 
@@ -41,7 +45,7 @@ db.dropDatabase();
 // House Arryn is not honorable!  Delete their motto.
 
 // your code begins here
-
+db.houses.deleteOne({motto:"As High as Honor" })
 // your code ends here
 
 
@@ -51,7 +55,7 @@ db.dropDatabase();
 // House Stark.
 
 // your code begins here
-
+db.houses.deleteOne({name: "House Stark"})
 // your code ends here
 
 
